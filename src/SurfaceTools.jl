58b3a15967f14d717ea36b0aad54af81ffb7dcfc
Josua Grawitter <josua.grawitter@campus.tu-berlin.de>
2019-02-26 17:44:08 +0100

    remove dependency on ApproxFun and ForwardDiff

--- src/SurfaceTools.jl
+++ src/SurfaceTools.jl
@@ -1,31 +1,10 @@
 module SurfaceTools
 
 using LinearAlgebra
-#using ForwardDiff
 using StaticArrays
-using ApproxFun
 using Calculus
 
-import Base: getindex
-export ApproxFunCompatible, getindex
-
 export tangentials, normal, gram_det, curvature
-
-struct ApproxFunCompatible{F, G, H} <: Function
-    fs::Tuple{F, G, H}
-end
-
-function (f::ApproxFunCompatible{F, G, H})(p::T) where {F, G, H, T}
-    return SVector(Tuple(f.fs[i](p) for i in 1:3))
-end
-
-getindex(f::A, i::Int) where A <: ApproxFunCompatible = f.fs[i]
-
-function tangentials(f::F, p) where F <: Union{Fun, ApproxFunCompatible}
-    J = @SMatrix [(Derivative(space(f[j]), i == 1 ? [1, 0] : [0, 1]) * f[j])(p)
-                  for j in 1:3, i in 1:2]
-    return J[:, 1], J[:, 2]
-end
 
 function tangentials(f, p)
     J = Calculus.jacobian(f, Vector(p), :central)
@@ -44,13 +23,6 @@ function _first_form(f, p)
     # we use StaticArrays to get fast inversion of 2x2 matrices
     return @SMatrix [t1 ⋅ t1  t1 ⋅ t2;
                      t1 ⋅ t2  t2 ⋅ t2]
-end
-
-function _hessian(f::F, p) where F <: Union{Fun, ApproxFunCompatible}
-    return Tuple(@SMatrix [(Derivative(space(f[k]),
-                        ifelse(i == 1, [1, 0], [0, 1]) .+
-                        ifelse(j == 1, [1, 0], [0, 1])) * f[k])(p)
-                  for i in 1:2, j in 1:2] for k in 1:3)
 end
 
 function _hessian(f, p)
--- test/runtests.jl
+++ test/runtests.jl
@@ -5,6 +5,6 @@ using LinearAlgebra
 
 @testset "sphere" begin
     s(p) = @SVector [p[1], p[2], sqrt(1 - p ⋅ p)]
-    @test curvature(s, @SVector [0., 0.]) ≈ -1
+    @test curvature(s, @SVector [0., 0.]) ≈ 1
     @test gram_det(s, @SVector [0., 0.]) ≈ 1
 end
